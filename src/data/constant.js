0f9836e98fa0fccd2f01d01d9e47cb299bfaee64
Mitesh Dhabekar <dev0330d@gmail.com>
2026-02-15 09:15:19 +0530

    New Update

--- src/data/constant.js
+++ src/data/constant.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/miteshdhabekar",
   resume:
-    "https://drive.google.com/file/d/1pmKKkX3Z9-U-pqQZa5hClfUyxm9LyHfQ/view?usp=sharing",
+    "https://drive.google.com/file/d/1ngmx3dY6dp4G7W6ba89VGcHFFkiuvj9B/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/miteshdhabekar/",
   twitter: "https://twitter.com/Dmitesh2003",
   insta: "https://www.instagram.com/d.mitu03/",
